fe5b797e56f1fde0f08ff0b750af66281d9033b8
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2025-09-08 14:14:54 +0200

    Create layout

--- app/src/main/java/com/example/planerwyprawy/MainActivity.kt
+++ app/src/main/java/com/example/planerwyprawy/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.planerwyprawydomordoru
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +21,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val raceSpinner: Spinner = findViewById<Spinner>(R.id.characterRaceSpinner)
+        val races = arrayOf("hobbit", "człowiek", "elf", "krasnolud", "czarodziej")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, races)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        raceSpinner.adapter = adapter
+
+        raceSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedRace = races[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                 val selectedRace = races[0]
+            }
+        }
+
+        // Jak coś zmienna z wybraną rasą to " selectedRace "
+
     }
 }
