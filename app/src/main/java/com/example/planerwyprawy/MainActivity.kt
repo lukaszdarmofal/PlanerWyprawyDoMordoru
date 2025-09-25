035915917fb54c53827260e1de5fcb4a82dea151
Mk <maciej.krzywda@cyfroweszkoly.pl>
2025-09-25 20:07:24 +0200

    Finalize the project

--- app/src/main/java/com/example/planerwyprawy/MainActivity.kt
+++ app/src/main/java/com/example/planerwyprawy/MainActivity.kt
@@ -6,18 +6,31 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Chronometer
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.RatingBar
 import android.widget.SeekBar
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.TimePicker
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.planerwyprawy.R
+import java.time.Month
+
 
 class MainActivity : AppCompatActivity() {
+    var selectedRace: String = ""
+    var output = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,6 +39,35 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val summaryText = findViewById<TextView>(R.id.summaryTextView)
+        val name = findViewById<EditText>(R.id.characterName).text
+        val elfPath = findViewById<Switch>(R.id.elfySwitch)
+        val moral = findViewById<RatingBar>(R.id.moraleRating)
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        val cloakCheckBox = findViewById<CheckBox>(R.id.equipmentCheckBox1)
+        val lembasCheckBox = findViewById<CheckBox>(R.id.equipmentCheckBox2)
+        val torchCheckBox = findViewById<CheckBox>(R.id.equipmentCheckBox3)
+
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
+
+        val year = datePicker.year
+        val month = datePicker.month
+        val day = datePicker.dayOfMonth
+
+        var path = ""
+        if (elfPath.isChecked == true) {
+            path = "tak"
+        } else path = "nie"
+
+        var march = ""
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId).text
+            march = "$radioButton"
         }
 
         val timePicker: TimePicker = findViewById<TimePicker>(R.id.timePicker)
@@ -51,11 +93,18 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                val selectedRace = races[position]
+                selectedRace = races[position]
+                when (position) {
+                    0 -> image.setImageResource(R.drawable.vaginal)
+                    1 -> image.setImageResource(R.drawable.sword)
+                    2 -> image.setImageResource(R.drawable.arrow)
+                    3 -> image.setImageResource(R.drawable.hammer)
+                    4 -> image.setImageResource(R.drawable.weapon)
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                 val selectedRace = races[0]
+                selectedRace = races[0]
             }
         }
 
@@ -67,7 +116,7 @@ class MainActivity : AppCompatActivity() {
         durationTimeSeekBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-                    val output = "" + seek.progress + " min"
+                    output = "" + seek.progress + " min"
                     durationTimeTexView.text = output
                 }
 
@@ -97,5 +146,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+
+        val summaryButton = findViewById<Button>(R.id.summaryButton)
+        summaryButton.setOnClickListener {
+            val equipment = mutableListOf<String>()
+            if (cloakCheckBox.isChecked) equipment.add("Płaszcz elfów")
+            if (lembasCheckBox.isChecked) equipment.add("Lembasy")
+            if (torchCheckBox.isChecked) equipment.add("Pochodnia")
+            summaryText.setText("Imię: "+name+"\nRasa: "+selectedRace+"\nŚcieżki elfów: "+path+"\nMorale: "+moral.rating + "\nCzas trwania marszu: "+output + "\nTermin: "+year+"."+month+"."+day+" "+selectedTime+"\n Ekwipunek: "+equipment.joinToString(" ,")+"\n Priorytet marszu: "+march.toString()) }
     }
 }
